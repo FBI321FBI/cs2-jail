0240fa582323ade992af1ee4a7e8e9bddc60dcd3
ilya.postnikov <im-popow13@mail.ru>
2026-03-23 23:50:17 +0500

    master

--- Jail.Common/Helpers/PlayerHelper.cs
+++ Jail.Common/Helpers/PlayerHelper.cs
@@ -1,5 +1,6 @@
 ﻿using CounterStrikeSharp.API;
 using CounterStrikeSharp.API.Core;
+using CounterStrikeSharp.API.Modules.Utils;
 
 namespace Jail.Common.Helpers;
 
@@ -24,4 +25,40 @@ public static class PlayerHelper
 
 		return player;
 	}
+
+	/// <summary>
+	/// Возвращает всех живых игроков.
+	/// </summary>
+	public static List<CCSPlayerController> GetAlivePlayers()
+		=> Utilities.GetPlayers().Where(x => x.PawnIsAlive).ToList();
+
+	/// <summary>
+	/// Возвращает всех живых Т игроков.
+	/// </summary>
+	public static List<CCSPlayerController> GetAliveTPlayers()
+		=> GetAlivePlayers().Where(x => x.Team == CsTeam.Terrorist).ToList();
+
+	/// <summary>
+	/// Возвращает всех живых КТ игроков.
+	/// </summary>
+	public static List<CCSPlayerController> GetAliveCTPlayers()
+		=> GetAlivePlayers().Where(x => x.Team == CsTeam.CounterTerrorist).ToList();
+
+	/// <summary>
+	/// Возвращает всех мёртвых игроков.
+	/// </summary>
+	public static List<CCSPlayerController> GetDeadPlayers()
+		=> Utilities.GetPlayers().Where(x => !x.PawnIsAlive).ToList();
+
+	/// <summary>
+	/// Возвращает всех мёртвых Т игроков.
+	/// </summary>
+	public static List<CCSPlayerController> GetDeadTPlayers()
+		=> GetDeadPlayers().Where(x => x.Team == CsTeam.Terrorist).ToList();
+
+	/// <summary>
+	/// Возвращает всех мёртвых КТ игроков.
+	/// </summary>
+	public static List<CCSPlayerController> GetDeadCTPlayers()
+		=> GetDeadPlayers().Where(x => x.Team == CsTeam.CounterTerrorist).ToList();
 }

bf1e811e5c89b98e9f45778e59ff980a589a9e07
ilya.postnikov <im-popow13@mail.ru>
2026-04-25 23:08:30 +0500

    master

--- Jail.Common/Helpers/UtilitiesHelper.cs
+++ Jail.Common/Helpers/UtilitiesHelper.cs
@@ -1,11 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using CounterStrikeSharp.API;
 using CounterStrikeSharp.API.Core;
 using CounterStrikeSharp.API.Modules.Utils;
-using CounterStrikeSharp.API;
 
 namespace Jail.Common.Helpers;
 
